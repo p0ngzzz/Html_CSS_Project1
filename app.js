73d16c79578030143ff9b1731cd5edd86589f4fd
p0ngzzz <pongsatornnea@gmail.com>
2023-11-18 19:34:36 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,41 @@
+const box1 = document.querySelector(".box-1");
+const doOverlay = document.querySelector(".do-overlay");
+const img_1 = document.querySelector(".img-1")
+var logo = document.querySelector(".logo");
+console.log(logo);
+// addEventListener
+window.addEventListener("scroll", handleScroll);
+
+// handle event function
+function change(e) {
+    console.log(e.type);
+    img_1.style.background = "pink";
+   
+}
+function changeBack(e) {
+    console.log(e.type);
+    img_1.style.background = "yellow";
+
+}
+
+function handleScroll() {
+    var scrollPosition = window.scrollY;
+    console.log(scrollPosition);
+    
+    // Check if scrolling down
+    if (scrollPosition > 0) {
+      
+        // Adjust the scale based on the scroll position
+        var scale = 1 - scrollPosition / 800; // You can adjust this factor
+        console.log(scale)
+          if( scale >= 0.1 ) {
+              logo.style.transform = "scale(" + scale + ")";
+          }
+        // Apply the scaling using the transform property
+      //   image.style.transform = "scale(" + scale + ")";
+      
+    }
+  }
+
+
+
